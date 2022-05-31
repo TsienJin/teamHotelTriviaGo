ae6c7b47d0084ee5404f4a23c11274eec08eebd7
Tsien Jin <tsien.jin@gmail.com>
2022-05-31 23:11:57 +0800

    Deployed test build to sanity studio

---
+++ backend/schemas/test.js
@@ -0,0 +1,12 @@
+export default{
+    name: 'testing',
+    type: 'document',
+    title: 'Test',
+    fields: [
+        {
+            name: 'name',
+            type: 'string',
+            title: 'Name'
+        }
+    ]
+}
