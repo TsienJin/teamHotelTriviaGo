c90f73e5fd4476053ebf0e18407724260689f6c2
Tsien Jin <tsien.jin@gmail.com>
2022-06-19 18:51:44 +0800

    init next.js with tailwindcss v3.0

---
+++ frontend/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <p className='w-full h-screen capitalize text-4xl font-semibold flex justify-center items-center'>Hello world!</p>
+    </>
+  )
+}
