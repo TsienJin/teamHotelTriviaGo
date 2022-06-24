0b11e55afe51233b1db2a236b294d1498fd1fb91
Tsien Jin <tsien.jin@gmail.com>
2022-06-24 12:27:42 +0800

    Update IndexSectionWrapper.jsx

--- frontend/components/IndexSectionWrapper.jsx
+++ frontend/components/IndexSectionWrapper.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function IndexSectionWrapper({bgColour="", headingColour="text-black", heading="", subHeading="", children}) {
   return (
-    <section className={`w-full py-8 sm:px-32 px-4 gap-y-8 flex flex-col justify-center items-center overflow-visible ${bgColour}`}>
+    <section className={`w-full pb-16 pt-12 sm:px-32 px-4 gap-y-8 flex flex-col justify-center items-center overflow-visible ${bgColour}`}>
       <div className={`${headingColour} text-center`}>
-        <h2 className='font-medium text-4xl'>{heading}</h2>
-        <span>{subHeading}</span>
+        <h2 className='font-medium text-3xl sm:text-5xl cursor-default'>{heading}</h2>
+        <h4 className='font-light text-2xl sm:text-2xl opacity-70 mt-2 cursor-default max-w-prose'>{subHeading}</h4>
       </div>
-      <div className=''>
+      <div className='w-full flex justify-center items-center'>
         {children}
       </div>
     </section>
