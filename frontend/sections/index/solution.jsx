dab506a2bd35035cdddbe969d092520c62b7cede
Tsien Jin <tsien.jin@gmail.com>
2022-06-24 12:27:46 +0800

    Update solution.jsx

--- frontend/sections/index/solution.jsx
+++ frontend/sections/index/solution.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 import IndexSectionWrapper from '../../components/IndexSectionWrapper'
@@ -5,21 +6,23 @@ import IndexSectionWrapper from '../../components/IndexSectionWrapper'
 
 function ButtonProductCTA({text="Link Button", link="/", target="_self"}){
     return(
-        <a href={link} target={target}>
-            <div className="
-            bg-green-600 text-white text-md text-center py-2 px-4 rounded-lg shadow shadow-green-300 w-full
-            transition-all
-            hover:bg-green-500 hover:rounded
-        ">
-                {text}
-            </div>
-        </a>
+        <Link href={link}>
+            <a>
+                <div className="
+                bg-green-600 text-white text-md text-center py-2 px-4 rounded-lg shadow shadow-green-300 w-full
+                transition-all
+                hover:bg-green-500 hover:rounded
+            ">
+                    {text}
+                </div>
+            </a>
+        </Link>
     )
 }
 
 
 
-function ProductFeature({text="feature"}){
+function ProductFeature({text="Click me!"}){
     return(
         <div className='flex justify-start items-center gap-x-2'>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 opacity-30" viewBox="0 0 20 20" fill="currentColor">
@@ -34,7 +37,7 @@ function ProductFeature({text="feature"}){
 
 function ProductCard({tier="", description="", features=[]}){
     return(
-        <div className='w-full h-full rounded-lg bg-white shadow-md'>
+        <div className='w-full h-full rounded-lg bg-white shadow-md max-w-sm'>
             <div className='p-6 pb-2 w-full'>
                 <h4 className='text-2xl font-semibold text-left mb-1'>{tier}</h4>
                 <p className='font-light text-gray-600'>{description}</p>
@@ -47,7 +50,7 @@ function ProductCard({tier="", description="", features=[]}){
                 })}
             </div>
             <div className="p-6 pt-0 pb-5 bg-gray-50 rounded-b-lg w-full">
-                <ButtonProductCTA text='Create account'/>
+                <ButtonProductCTA text='Try it now!'/>
             </div>
         </div>
     )
@@ -60,15 +63,25 @@ export default function SectionProuct({props}){
     const features = {
         standard: ['Keyword analysis'],
         elite: ['Keyword analysis', 'Automatic calculations'],
-        supreme: ['Keyword analysis', 'Automatic calculations', 'Customisable results', 'Automated MD&A drafting'],
+        supreme: ['Keyword analysis', 'Automatic calculations', 'Customisable field selection', 'Automated MD&A drafting'],
+    }
+
+    const headingText = {
+        heading:
+            "Faster workflows",
+        subHeading:
+            "Save man hours, automate your processes with us",
+    }
+
+    const productDescription = {
+        supreme: 
+            "Easily automate the boring stuff using machine learning, stay focused on what you do best.",
     }
 
     return(
-        <IndexSectionWrapper heading='Our Solutions' subHeading='' bgColour='bg-blue-900' headingColour='text-white'>
-            <div className='flex flex-col w-full gap-8 sm:flex-row'>
-                <ProductCard tier={"Standard user"} features={features.standard} description={"Best for small businesses and students."}/>
-                <ProductCard tier={"Elite user"} features={features.elite} description={"Built for businesses that value time."}/>
-                <ProductCard tier={"Supreme user"} features={features.supreme} description={"Most powerful liscence available."}/>
+        <IndexSectionWrapper heading={headingText.heading} subHeading={headingText.subHeading} bgColour='bg-blue-900' headingColour='text-white'>
+            <div className='flex flex-col w-full gap-8 justify-center items-center'>
+                <ProductCard tier={"Powered"} features={features.supreme} description={productDescription.supreme}/>
             </div>
         </IndexSectionWrapper>
     )
