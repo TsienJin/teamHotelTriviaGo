923c3eadc0ccca81924a73367a1b28cf8db6cced
Tsien Jin <tsien.jin@gmail.com>
2022-06-24 13:43:10 +0800

    Fixed styling

--- frontend/sections/socialProof.jsx
+++ frontend/sections/socialProof.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import IndexSectionWrapper from '../components/IndexSectionWrapper'
 
-
+import {useRouter} from 'next/router'
 
 
 function ImageContainter({props}){
 
+  // <a href="https://unsplash.com/photos/pAtA8xe_iVM" target={"_blank"} rel="noopener noreferrer">
+
+  const router = useRouter()
+  const handleClick = e => {
+    e.preventDefault()
+    window.open("https://unsplash.com/photos/pAtA8xe_iVM")
+  }
+
   return(
-    <div className="relative w-full sm:max-w-md sm:-mt-16 sm:-mb-16 cursor-pointer">
-      <a href="https://unsplash.com/photos/pAtA8xe_iVM" target={"_blank"} className='w-full relative over'>
-        <img className='rounded-t-lg sm:rounded-lg sm:shadow-md object-cover object-top w-full max-h-[50vh] sm:max-h-[55vh]' src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8" alt="" />
-        <div className='rounded-t-lg sm:rounded-lg absolute z-10 top-0 bottom-0 left-0 right-0 rounded-lg bg-gradient-to-t from-slate-700 to-transparent via-transparent opacity-70'></div>
-        <div className="absolute bottom-4 left-8 sm:bottom-8 sm:left-8 z-20">
-          <h5 className='font-semibold capitalize text-xl text-white'>Mike Wazowski</h5>
-          <span className='font-light text-md text-white'>Partner, Legal Clinic Inc.</span>
-        </div>
-      </a>
+    <div className="min-h-[40vh] sm:w-1/2 sm:ml-12 sm:-mt-16 sm:-mb-16 sm:min-h-[60vh] sm:max-w-[30vw] bg-cover bg-top rounded-lg relative overflow-hidden
+    bg-[url(https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8)]" onClick={e=>{handleClick(e)}}>
+      <div className='absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-t from-slate-900 to-transparent via-transparent opacity-80'></div>
+      <div className="absolute bottom-6 left-6">
+        <p className='font-semibold capitalize text-xl text-white'>Mike Wazowski</p>
+        <p className='font-light text-md text-white'>Partner, Legal Clinic Inc.</p>
+      </div>
     </div>
   )
 }
@@ -25,7 +31,7 @@ function Testimony({props}){
   const testimonial = `"Our workflow is so much faster and accurate, our turnover rate as dramatically increased since we started using this product. What a game changer!"`
 
   return(
-    <div className='p-8 w-full h-full flex flex-col justify-start flex-1 flex-grow sm:w-1/2'>
+    <div className='p-8 w-full h-full flex flex-col justify-start flex-1 flex-grow'>
       <p className='italic text-slate-700 font-light text-xl sm:text-2xl sm:font-extralight'>{testimonial}</p>
     </div>
   )
@@ -44,7 +50,7 @@ function Testimony({props}){
 export default function SocialProof() {
   return (
     <IndexSectionWrapper bgColour='bg-gray-100' heading='Testimonial' subHeading='Hear it from industry experts'>
-        <div className='flex flex-col sm:flex-row sm:justify-center sm:items-center rounded-lg overflow-visable bg-white shadow-lg w-full sm:pl-12 sm:my-16 sm:mt-24'>
+        <div className='bg-white flex flex-col sm:flex-row sm:my-24 sm:justify-center sm:items-center'>
             <ImageContainter />
             <Testimony />
         </div>
