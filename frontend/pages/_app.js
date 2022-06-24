bd3e4eaec2774face1c6fce039d6fbc605182616
Tsien Jin <tsien.jin@gmail.com>
2022-06-24 12:33:00 +0800

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,15 @@
+import Footer from '../components/common/Footer'
+import Header from '../components/common/Header'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
 }
 
 export default MyApp
