eabedd86491f7fa71fc2eae765e929a00c8f069f
Tsien Jin <tsien.jin@gmail.com>
2022-06-24 23:43:08 +0800

    Added file upload field

--- frontend/pages/solution/index.jsx
+++ frontend/pages/solution/index.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import FileUpload from '../../sections/solution/fileUpload'
+import FilePreview from '../../sections/solution/filePreview'
+import IndexSectionWrapper from '../../components/IndexSectionWrapper'
 
 export default function solutionPage() {
+
+  const [uploaded, setUploaded] = useState(false)
+  const updateFileIsUploaded = () => {
+    setUploaded(true)
+  }
+  const updateFileIsCleared = () => {
+    setUploaded(false)
+  }
+
   return (
-    <div>solutionPage</div>
+    <IndexSectionWrapper heading='Upload your PDF file' subHeading='Do not upload any sensitive or privalaged information as this is a proof of concept' bgColour='bg-gradient-to-tl from-blue-800 to-indigo-500' headingColour='text-white'>
+      <div className='w-full flex flex-col justify-center items-center'>
+        {uploaded?<FilePreview method={updateFileIsCleared}/>:<FileUpload method={updateFileIsUploaded}/>}
+      </div>
+    </IndexSectionWrapper>
   )
 }
---
+++ frontend/sections/solution/filePreview.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import IndexSectionWrapper from '../../components/IndexSectionWrapper'
+
+export default function FilePreview() {
+  return (
+    <div>FilePreview</div>
+  )
+}
