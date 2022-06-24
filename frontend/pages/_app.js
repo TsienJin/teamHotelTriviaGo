dffd9b1d569cbfaf73120f06fca6e57877a27175
Tsien Jin <tsien.jin@gmail.com>
2022-06-24 23:42:45 +0800

    Added head tag

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 import Footer from '../components/common/Footer'
 import Header from '../components/common/Header'
 import '../styles/globals.css'
@@ -5,6 +7,10 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return(
     <>
+      <Head>
+        <title>Team Hotel Trivia Go</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head> 
       <Header />
       <Component {...pageProps} />
       <Footer />
