860373ed7d451e2f1fb1b6ce7b665cc3bdec6ec1
Tsien Jin <tsien.jin@gmail.com>
2022-06-26 00:41:31 +0800

    Changed button word

--- frontend/sections/solution/fileUpload.jsx
+++ frontend/sections/solution/fileUpload.jsx
@@ -81,7 +81,7 @@ function FileSubmitButton({methodSubmit=()=>{console.log('Method missing! FileSu
                 Clear files
             </button>
             <button onClick={methodSubmit} className='flex flex-row gap-x-1 bg-green-600 text-white px-4 py-2 rounded shadow border-2 border-green-600 hover:bg-green-500 hover:border-green-500 transition-colors z-50'>
-                Preview
+                Submit
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
                 </svg>
