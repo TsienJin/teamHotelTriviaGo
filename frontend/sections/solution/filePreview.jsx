aea21fa34199fc539df8d14a4dd823b8335bb95c
imevahans <ImEvaHans@gmail.com>
2022-06-25 08:12:05 +0800

    Created new files

--- frontend/sections/solution/filePreview.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import IndexSectionWrapper from '../../components/IndexSectionWrapper'
-
-export default function FilePreview() {
-  return (
-    <div>FilePreview</div>
-  )
-}
