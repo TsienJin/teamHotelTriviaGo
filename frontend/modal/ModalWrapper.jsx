5435e1516cab5027457df530ac2f728a27c5a410
Tsien Jin <tsien.jin@gmail.com>
2022-06-26 00:41:24 +0800

    Update ModalWrapper.jsx

--- frontend/modal/ModalWrapper.jsx
+++ frontend/modal/ModalWrapper.jsx
@@ -21,7 +21,6 @@ export default function ModalWrapper({children, closeMethod=()=>{}}) {
       <div onClick={closeMethod} className='bg-slate-400 bg-opacity-70 backdrop-saturate-[0.8] backdrop-blur-md fixed bottom-0 top-0 left-0 right-0 z-[100] flex flex-col justify-center items-center sm:px-16 sm:py-12'>
         {children}
       </div>
-      
     </>
   )
 }
