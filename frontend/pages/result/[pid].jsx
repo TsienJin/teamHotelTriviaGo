5e585b8b70f79d4df4415413388ecf6da200711f
Tsien Jin <tsien.jin@gmail.com>
2022-06-26 08:32:00 +0800

    Create [pid].jsx

---
+++ frontend/pages/result/[pid].jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { useRouter } from 'next/router'
+
+export default function ResultPage({props}) {
+
+    const router = useRouter()
+    const { pid } = router.query
+
+
+  return (
+    <div>{pid}</div>
+  )
+}
