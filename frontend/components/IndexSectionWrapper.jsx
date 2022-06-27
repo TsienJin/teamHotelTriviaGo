a9665be7034ee77f122d8ad249c4afe932670d95
Tsien Jin <tsien.jin@gmail.com>
2022-06-27 20:29:36 +0800

    Formatted heading and subheading if not present

--- frontend/components/IndexSectionWrapper.jsx
+++ frontend/components/IndexSectionWrapper.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function IndexSectionWrapper({bgColour="", headingColour="text-black", heading="", subHeading="", children}) {
   return (
     <section className={`w-full pb-16 pt-12 sm:px-32 px-4 gap-y-16 flex flex-col justify-center items-center overflow-visible ${bgColour}`}>
-      <div className={`${headingColour} text-center`}>
-        <h2 className='font-medium text-3xl sm:text-5xl cursor-default'>{heading}</h2>
-        <h4 className='font-light text-2xl sm:text-2xl opacity-70 mt-2 cursor-default max-w-prose'>{subHeading}</h4>
+      <div className={`${headingColour} text-center ${(heading.length==0 && subHeading.length==0)?"hidden":""}`}>
+        {heading.length>0 && <h2 className='font-medium text-3xl sm:text-5xl cursor-default'>{heading}</h2>}
+        {subHeading.length>0 && <h4 className='font-light text-2xl sm:text-2xl opacity-70 mt-2 cursor-default max-w-prose'>{subHeading}</h4>}
       </div>
       <div className='w-full flex justify-center items-center'>
         {children}
