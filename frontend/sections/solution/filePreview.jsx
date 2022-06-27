fda5d526617e6daa25d1e8199e4e6484004a95ab
Tsien Jin <tsien.jin@gmail.com>
2022-06-27 20:29:14 +0800

    Update filePreview.jsx

--- frontend/sections/solution/filePreview.jsx
+++ frontend/sections/solution/filePreview.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import IndexSectionWrapper from '../../components/IndexSectionWrapper'
-
 export default function FilePreview() {
   return (
     <div>FilePreview</div>
