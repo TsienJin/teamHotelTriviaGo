1fd444586c351c4407afd71d2e38c6e1c77d97e1
Tsien Jin <tsien.jin@gmail.com>
2022-06-29 09:30:25 +0800

    Removed SSR

--- frontend/pages/solution/index.jsx
+++ frontend/pages/solution/index.jsx
@@ -1,54 +1,33 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-const { MongoClient } = require("mongodb")
+// const { MongoClient } = require("mongodb")
 
 import FileUpload from '../../sections/solution/fileUpload'
 import FilePreview from '../../sections/solution/filePreview'
 import IndexSectionWrapper from '../../components/IndexSectionWrapper'
 import FileHistory from '../../sections/solution/fileHistory'
 
-export default function SolutionPage({ fileHistory=[] }) {
+// import cacheReq from './cacheRequest/cacheReq'
 
-  const [uploaded, setUploaded] = useState(false)
-  const updateFileIsUploaded = () => {
-    setUploaded(true)
-  }
-  const updateFileIsCleared = () => {
-    setUploaded(false)
-  }
+export default function SolutionPage({}) {
+
+  const [fileHistory, setFileHistory] = useState([])
+  
+  useEffect(()=>{
+    axios.get('/api/solution/file-history',{}).then(res=>{setFileHistory(res.data.data)})
+    // setFileHistory(cacheReq('/api/solution/file-history',{}))
+  },[])
+
 
   return (
     <>
       <IndexSectionWrapper heading='Upload your PDF files' subHeading='Do not upload any sensitive or privalaged information as this is a proof of concept' bgColour='bg-gradient-to-tl from-blue-800 to-indigo-500' headingColour='text-white'>
-        <FileUpload method={updateFileIsUploaded}/>
+        <FileUpload />
       </IndexSectionWrapper>
-      <IndexSectionWrapper bgColour='bg-slate-50' heading='' subHeading={`Previous file analysis, showing last ${fileHistory.length} jobs`}>
+      <IndexSectionWrapper bgColour={`bg-slate-50 ${fileHistory==0?"cursor-progress":""}`} heading='' subHeading={fileHistory.length>0?`Previous file analysis, showing the latest ${fileHistory.length} jobs`:"Loading history..."} >
         <FileHistory fileHistory={fileHistory}/>
       </IndexSectionWrapper>
     </>
   )
-}
-
-export async function getServerSideProps(){
-
-  const client = new MongoClient(process.env.MONGO_URI)
-  const result = []
-
-  try {
-    await client.connect()
-    const dbCur = client.db("HotelTriviaGodb").collection("mdna_test").find({},{projection:{_id:0, usrPassword:0}}).sort({_id:-1}).limit(20)
-    await dbCur.forEach(item=>{
-      result.push(item)
-    })
-  } catch (err) {
-    console.log(err)
-  } finally {
-    await client.close()
-  }
-
-  return {
-    props: {
-      fileHistory: result
-    }
-  }
 }
