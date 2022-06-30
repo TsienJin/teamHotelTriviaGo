d8bb960bc0a7c3448a6933553e8ed9346a911737
Tsien Jin <tsien.jin@gmail.com>
2022-07-01 00:08:03 +0800

    Fixed class -> className

--- frontend/components/result/result.jsx
+++ frontend/components/result/result.jsx
@@ -57,7 +57,7 @@ function FileWrapper({fileName="", statements=[]}){
     <div className='w-full max-w-prose bg-white rounded-lg  overflow-hidden'>
       <div className='topBar flex flex-row gap-2 justify-start items-center p-2'>
         <span onClick={handleExpand} className={`cursor-pointer transition-all opacity-30 sm:hover:opacity-70 ${isExpand?"rotate-90 sm:opacity-70":""}`}>
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
           </svg>
         </span>
