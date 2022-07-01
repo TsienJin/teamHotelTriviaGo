d89a18ccd592b5515dfab1432ee7009e39a7f47c
Tsien Jin <tsien.jin@gmail.com>
2022-07-01 08:37:03 +0800

    Uncommented section to send to flask server

--- frontend/pages/api/solution/submit-file-2.js
+++ frontend/pages/api/solution/submit-file-2.js
@@ -74,11 +74,11 @@ handler.post(async (req, res) => {
       formToSend.append('files', fs.createReadStream(files.files.filepath), files.files.originalFilename)
     }
 
-    // const result = await axios.post(
-    //   process.env.API_URL_FILEUPLOAD,
-    //   formToSend,
-    //   { headers: { 'Content-Type': 'multipart/form-data' } }
-    // );
+    const result = await axios.post(
+      process.env.API_URL_FILEUPLOAD,
+      formToSend,
+      { headers: { 'Content-Type': 'multipart/form-data' } }
+    );
 
   } catch (err) {
     console.log(err)
