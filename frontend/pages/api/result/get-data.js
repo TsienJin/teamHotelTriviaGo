2dcbee692ba34facd0a2ca5a13ac153fff22c485
Tsien Jin <tsien.jin@gmail.com>
2022-07-02 10:27:18 +0800

    fetching new data

--- frontend/pages/api/result/get-data.js
+++ frontend/pages/api/result/get-data.js
@@ -17,13 +17,14 @@ export default async function handler(req, res) {
     
     try {
         await client.connect()
-        const result = await client.db("HotelTriviaGodb").collection("mdna_test").findOne({sessionToken: req.body.pid}, {projection: {usrPassword: 1, isComplete: 1, mdna: 1,_id:0}})
+        const result = await client.db("HotelTriviaGodb").collection("mdna_test").findOne({sessionToken: req.body.pid}, {projection: {usrPassword: 1, isComplete: 1, mdna: 1, usrKeyword:1 ,_id:0}})
         // passwordMatch = bcrypt.compareSync(req.body.usrPassword, result.usrPassword)
         // console.log(result)
 
         if((bcrypt.compareSync(req.body.usrPassword, result.usrPassword) || (req.body.usrPassword.length==0 && result.usrPassword.length==0) || req.body.usrPassword === 'McGriddles') && result.isComplete){
             payload = {
-                mdna: result.mdna
+                mdna: result.mdna,
+                keywords: result.usrKeyword
             }
         }
         
