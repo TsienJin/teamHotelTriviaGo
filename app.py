82b537cf1c6110c624df446f34378bba0c5e48d8
Toh Jing Hua <tohjinghua123@gmail.com>
2022-07-02 10:29:12 +0800

    support multiple files upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from pprint import pprint
 
 import os
 
@@ -17,19 +18,25 @@ celery = make_celery(app)
 
 
 @celery.task()
-def cal_results(user_keyword, user_file, session_token):
-    res = generate_mda_main(user_keyword, user_file)
-    print(res)
-    db = connect_db()
-    mdna_collection = connect_collection(db)
-    data = mdna_collection.update_one({"sessionToken": session_token}, {
-        "$set": {
-            'mdna': res['mdna'],
-            'isComplete': res['isComplete'],
-            'isError': res['isError']
-        }
-    })
-    print(data)
+def cal_results(user_keyword, user_files, session_token):
+    output = {'mdna': {}, 'isComplete': True, 'isError': True}
+    for user_file in user_files:
+        res = generate_mda_main(user_keyword, user_file)
+        output['mdna'] = {**output['mdna'], **res['mdna']}
+        output['isComplete'] = output['isComplete'] and res['isComplete']
+        output['isError'] = output['isError'] and res['isError']
+
+    pprint(output)
+    # db = connect_db()
+    # mdna_collection = connect_collection(db)
+    # data = mdna_collection.update_one({"sessionToken": session_token}, {
+    #     "$set": {
+    #         'mdna': output['mdna'],
+    #         'isComplete': output['isComplete'],
+    #         'isError': output['isError']
+    #     }
+    # })
+    # print(data)
 
 
 @app.route('/')
@@ -49,19 +56,19 @@ def generate_mda():
         if ('files' not in request.files):
             return jsonify({"message": 'No files'}), 400
 
-        user_file = request.files['files']
+        user_files = request.files.getlist("files")
         session_token = request.form.get("sessionToken")
         user_password = request.form.get("usrPassword")
         user_keyword = request.form.get("usrKeyword")
         time = request.form.get("time")
 
-        print('user_file:', user_file)
+        print('user_files:', user_files)
         print('session token:', session_token)
         print('user_password:', user_password)
         print('user_keyword:', user_keyword)
         print('time:', time)
 
-        res = cal_results(user_keyword, user_file, session_token)
+        res = cal_results(user_keyword, user_files, session_token)
 
         return jsonify({
             "message": 'files received',
